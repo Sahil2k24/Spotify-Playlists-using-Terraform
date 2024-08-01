e2b2bec555da05dd3f96878832c8e3ce2568facf
SahilBrightLink <sahil.patil@brightlinkinfotechnologies.com>
2024-08-01 16:21:04 +0530

    new tracks added

--- playlist.tf
+++ playlist.tf
@@ -1,8 +1,16 @@
 resource "spotify_playlist" "Bollywood-Songs" {
-    name= "SAHIL's Playlist"
+    name= "Geeky Playlist"
     tracks= ["3vVIhgkDoC0vRBba5drHPe"]
 }
 
 data "spotify_search_track" "my-songs" {
     artist= "Arijit Singh"
+}
+
+resource "spotify_playlist" "my-playlist" {
+    name= "SAHIL's Playlist"
+    tracks= [data.spotify_search_track.my-songs.tracks[1].id,
+    data.spotify_search_track.my-songs.tracks[2].id,
+    data.spotify_search_track.my-songs.tracks[3].id
+    ]
 }

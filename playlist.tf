86fc702c3771895deed726649b63d132609c1bbc
SahilBrightLink <sahil.patil@brightlinkinfotechnologies.com>
2024-08-01 16:24:51 +0530

    new songs added

--- playlist.tf
+++ playlist.tf
@@ -11,5 +11,10 @@ resource "spotify_playlist" "my-playlist" {
     name= "SAHIL's Playlist"
     tracks= [data.spotify_search_track.my-songs.tracks[0].id,
     data.spotify_search_track.my-songs.tracks[1].id,
-    data.spotify_search_track.my-songs.tracks[2].id]
+    data.spotify_search_track.my-songs.tracks[2].id,
+    data.spotify_search_track.my-songs.tracks[3].id,
+    data.spotify_search_track.my-songs.tracks[4].id,
+    data.spotify_search_track.my-songs.tracks[5].id,
+    data.spotify_search_track.my-songs.tracks[6].id,
+    data.spotify_search_track.my-songs.tracks[7].id]
 }

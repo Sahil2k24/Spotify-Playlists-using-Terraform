d6231d4cd94c8ba2973ccacfd8e7abdc8de46663
SahilBrightLink <sahil.patil@brightlinkinfotechnologies.com>
2024-08-01 16:22:34 +0530

    new changes

--- playlist.tf
+++ playlist.tf
@@ -9,8 +9,7 @@ data "spotify_search_track" "my-songs" {
 
 resource "spotify_playlist" "my-playlist" {
     name= "SAHIL's Playlist"
-    tracks= [data.spotify_search_track.my-songs.tracks[1].id,
-    data.spotify_search_track.my-songs.tracks[2].id,
-    data.spotify_search_track.my-songs.tracks[3].id
-    ]
+    tracks= [data.spotify_search_track.my-songs.tracks[0].id,
+    data.spotify_search_track.my-songs.tracks[1].id,
+    data.spotify_search_track.my-songs.tracks[2].id]
 }

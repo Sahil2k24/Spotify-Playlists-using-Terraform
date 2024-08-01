b2419d48b069a87d9b8264e4b40bbc705aebd7ff
SahilBrightLink <sahil.patil@brightlinkinfotechnologies.com>
2024-08-01 16:13:52 +0530

    data block updated

--- playlist.tf
+++ playlist.tf
@@ -2,3 +2,7 @@ resource "spotify_playlist" "Bollywood-Songs" {
     name= "SAHIL's Playlist"
     tracks= ["3vVIhgkDoC0vRBba5drHPe"]
 }
+
+data "spotify_search_track" "my-songs" {
+    artist= "Arijit Singh"
+}
